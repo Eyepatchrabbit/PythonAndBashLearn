ac75d02a1d5e3c396a8f8793b20a3e8c5ad733b5
alexanderboffin <boffin.alexander@gmail.com>
2019-07-20 11:10:56 +0200

    py01 - start udemy lesson

---
+++ pythonexecutables/udemycoursebasetoAdvance/Basis.py
@@ -0,0 +1,30 @@
+
+#my first element to see if all works
+print('hello World')
+
+#variables:
+print("\nVariables:")
+
+var=37.86
+
+print(var)
+
+var1=var
+
+var1=100
+
+print("var="+str(var), "var1= "+ str(var1))
+
+print("\nSpecial Variables:")
+
+varSpe=3-1j
+varSpe1=2-5j
+
+print(varSpe+varSpe1)
+
+print("\nStr part")
+alfabeth="abcdefghijklmnopqrstuvwxyz"
+print(alfabeth)
+print("alfabeth[0]="+ alfabeth[0])
+print("alfabeth[9]="+ alfabeth[9])
+print("alfabeth[-1]="+ alfabeth[-1])
